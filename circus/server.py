e8438f97642c4a36e35725a24109831a3faa8f12
benoitc <bchesneau@gmail.com>
2012-02-17 03:42:06 +0100

    add daemon & pidfile support

--- circus/server.py
+++ circus/server.py
@@ -5,11 +5,18 @@ import argparse
 import ConfigParser
 import os
 import logging
+import resource
 
 from circus import logger
 from circus.trainer import Trainer
 from circus.show import Show
+from circus.pidfile import Pidfile
 
+MAXFD = 1024
+if (hasattr(os, "devnull")):
+   REDIRECT_TO = os.devnull
+else:
+   REDIRECT_TO = "/dev/null"
 
 LOG_LEVELS = {
     "critical": logging.CRITICAL,
@@ -34,6 +41,44 @@ class DefaultConfigParser(ConfigParser.ConfigParser):
         else:
             raise NotImplementedError()
 
+def get_maxfd():
+    maxfd = resource.getrlimit(resource.RLIMIT_NOFILE)[1]
+    if (maxfd == resource.RLIM_INFINITY):
+        maxfd = MAXFD
+    return maxfd
+
+try:
+    from os import closerange
+except ImportError:
+    def closerange(fd_low, fd_high):
+        # Iterate through and close all file descriptors.
+        for fd in xrange(fd_low, fd_high):
+            try:
+                os.close(fd)
+            except OSError:	# ERROR, fd wasn't open to begin with (ignored)
+                pass
+
+def daemonize():
+    """\
+    Standard daemonization of a process.
+    http://www.svbug.com/documentation/comp.unix.programmer-FAQ/faq_2.html#SEC16
+    """
+
+    #if not 'CIRCUS_PID' in os.environ:
+    if os.fork():
+        os._exit(0)
+    os.setsid()
+
+    if os.fork():
+        os._exit(0)
+
+    os.umask(0)
+    maxfd = get_maxfd()
+    closerange(0, maxfd)
+
+    os.open(REDIRECT_TO, os.O_RDWR)
+    os.dup2(0, 1)
+    os.dup2(0, 2)
 
 def main():
     parser = argparse.ArgumentParser(description='Run some shows.')
@@ -42,9 +87,26 @@ def main():
             help="log level")
     parser.add_argument('--log-output', dest='logoutput', default='-',
             help="log output")
+    parser.add_argument('--daemon', dest='daemonize', action='store_true',
+            help="Start circusd in the background")
+    parser.add_argument('--pidfile', dest='pidfile')
+
     args = parser.parse_args()
     cfg = DefaultConfigParser()
     cfg.read(args.config)
+
+    if args.daemonize:
+        daemonize()
+
+    pidfile = None
+    if args.pidfile:
+        pidfile = Pidfile(args.pidfile)
+
+        try:
+            pidfile.create(os.getpid())
+        except RuntimeError, e:
+            print(str(e))
+            sys.exit(1)
 
     # configure the logger
     loglevel = LOG_LEVELS.get(args.loglevel.lower(), logging.INFO)
@@ -87,6 +149,8 @@ def main():
         trainer.run()
     finally:
         trainer.terminate()
+        if pidfile is not None:
+            pidfile.unlink()
 
 
 if __name__ == '__main__':
