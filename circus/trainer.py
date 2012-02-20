1e512c08e31f6e8ee212edb0930241384735a8d5
benoitc <bchesneau@gmail.com>
2012-02-20 20:55:50 +0100

    remove headers and NOTIICE
    licensing is still in discussion.

--- circus/trainer.py
+++ circus/trainer.py
@@ -1,9 +1,3 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.  The
-# Secondary License is the Apache License 2.0. You may obtain a copy of
-# the Apache License 2 at http://www.apache.org/licenses/LICENSE-2.0.
-
 import os
 import sys
 
