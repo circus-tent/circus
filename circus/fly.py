970920c371d00f527fc358c161306569ac6ab39b
Tarek Ziade <tarek@ziade.org>
2012-02-24 15:03:10 +0100

    now using a custom Popen class with a get_info() method

--- circus/fly.py
+++ circus/fly.py
@@ -1,65 +1,14 @@
-from psutil import Popen
-from psutil.error import AccessDenied
 import os
 from pwd import getpwnam
 from grp import getgrnam
-from datetime import timedelta
 import time
 
 from circus import logger
+from circus.util import Popen
 
 
-def bytes2human(n):
-    """
-    >>> bytes2human(10000)
-    '9K'
-    >>> bytes2human(100001221)
-    '95M'
-    """
-
-    if not isinstance(n, int):
-        return n
-
-    symbols = ('K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y')
-    prefix = {}
-    for i, s in enumerate(symbols):
-        prefix[s] = 1 << (i+1)*10
-    for s in reversed(symbols):
-        if n >= prefix[s]:
-            value = int(float(n) / prefix[s])
-            return '%s%s' % (value, s)
-    return "%sB" % n
-
-def info_line(p):
-    try:
-        mem_info = p.get_memory_info()
-    except AccessDenied:
-        mem_info = ("N/A", "N/A")
-
-    try:
-        cpu = p.get_cpu_percent(interval=0)
-    except AccessDenied:
-        cpu = "N/A"
-
-    try:
-        mem = round(p.get_memory_percent(), 1)
-    except AccessDenied:
-        mem = "N/A"
-
-    try:
-        cpu_times = p.get_cpu_times()
-
-        ctime = timedelta(seconds=sum(cpu_times))
-        ctime = "%s:%s.%s" % (ctime.seconds // 60 % 60,
-                          str((ctime.seconds % 60)).zfill(2),
-                          str(ctime.microseconds)[:2])
-
-    except AccessDenied:
-        ctime = "N/A"
-
-    return "%s %s %s %s %s %s %s %s" % (p.pid, p.username, p.nice,
-            bytes2human(mem_info[1]), bytes2human(mem_info[0]), cpu,
-            mem, ctime)
+_INFOLINE = ("%(pid)s %(username)s %(nice)s %(mem_info1)s "
+             "%(mem_info2)s %(cpu)s %(mem)s %(ctime)s")
 
 
 class Fly(object):
@@ -103,39 +52,31 @@ class Fly(object):
 
     def info(self):
         """ return process info """
-        lines = ["%s: %s" % (self.wid, info_line(self._worker))]
-        try:
-            for child in self._worker.get_children():
-                lines.append("   %s" % info_line(child))
-        except AccessDenied:
-            pass
+        info = _INFOLINE % self._worker.get_info()
+        lines = ["%s: %s" % (self.wid, info)]
+
+        for child in self._worker.get_children():
+            info = _INFOLINE % child.get_info()
+            lines.append("   %s" % info)
+
         return "\n".join(lines)
 
     def children(self):
-        try:
-            return ",".join(["%s" % child.pid \
-                    for child in self._worker.get_children()])
-        except AccessDenied:
-            return "error: access denied"
+        return ",".join(["%s" % child.pid
+                         for child in self._worker.get_children()])
 
     def send_signal_child(self, pid, signum):
-        try:
-            pids = [child.pid for child in self._worker.get_children()]
-            if pid in pids:
-                child.send_signal(signum)
-                return "ok"
-            else:
-                return "error: child not found"
-        except AccessDenied:
-            return "error: access denied"
+        pids = [child.pid for child in self._worker.get_children()]
+        if pid in pids:
+            child.send_signal(signum)
+            return "ok"
+        else:
+            return "error: child not found"
 
     def send_signal_children(self, signum):
-        try:
-            for child in self._worker.get_children():
-                child.send_signal(signum)
-            return "ok"
-        except AccessDenied:
-            return "error: access denied"
+        for child in self._worker.get_children():
+            child.send_signal(signum)
+        return "ok"
 
     @property
     def pid(self):
---
+++ circus/util.py
@@ -0,0 +1,68 @@
+from datetime import timedelta
+
+from psutil.error import AccessDenied
+from psutil import Popen as PSPopen
+
+
+_SYMBOLS = ('K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y')
+
+
+def bytes2human(n):
+    """
+    >>> bytes2human(10000)
+    '9K'
+    >>> bytes2human(100001221)
+    '95M'
+    """
+    if not isinstance(n, int):
+        return n
+
+    prefix = {}
+    for i, s in enumerate(_SYMBOLS):
+        prefix[s] = 1 << (i + 1) * 10
+
+    for s in reversed(_SYMBOLS):
+        if n >= prefix[s]:
+            value = int(float(n) / prefix[s])
+            return '%s%s' % (value, s)
+    return "%sB" % n
+
+
+class Popen(PSPopen):
+    def get_info(self):
+        info = {}
+        try:
+            mem_info = self.get_memory_info()
+            info['mem_info1'] = bytes2human(mem_info[0])
+            info['mem_info2'] = bytes2human(mem_info[1])
+        except AccessDenied:
+            info['mem_info1'] = info['mem_info2'] = "N/A"
+
+        try:
+            info['cpu'] = self.get_cpu_percent(interval=0)
+        except AccessDenied:
+            info['cpu'] = "N/A"
+
+        try:
+            info['mem'] = round(self.get_memory_percent(), 1)
+        except AccessDenied:
+            info['mem'] = "N/A"
+
+        try:
+            cpu_times = self.get_cpu_times()
+            ctime = timedelta(seconds=sum(cpu_times))
+            ctime = "%s:%s.%s" % (ctime.seconds // 60 % 60,
+                            str((ctime.seconds % 60)).zfill(2),
+                            str(ctime.microseconds)[:2])
+        except AccessDenied:
+            ctime = "N/A"
+
+        info['ctime'] = ctime
+
+        for name in ('pid', 'username', 'nice'):
+            try:
+                info[name] = getattr(self, name)
+            except AccessDenied:
+                info[name] = 'N/A'
+
+        return info
