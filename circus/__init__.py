f04586fd76ccfd580a5259cac8a7bc74aec3cc35
Tarek Ziade <tarek@ziade.org>
2012-03-12 16:27:04 -0700

    Revert "add a possibility to specify the ipc_path in the trainer"
    This reverts commit 49810aec196e03f7b2331a9c6fb18c313c7c2c5b.

--- circus/__init__.py
+++ circus/__init__.py
@@ -8,7 +8,7 @@ logger = logging.getLogger('circus')
 def get_trainer(cmd, num_workers=1., timeout=1.0, check=5.,
                 warmup_delay=0., controller='tcp://127.0.0.1:5555',
                 shell=False, working_dir=None, uid=None, gid=None,
-                env=None, name=None, ipc_path=None):
+                env=None, name=None):
     from circus.show import Show
     from circus.trainer import Trainer
 
@@ -18,4 +18,4 @@ def get_trainer(cmd, num_workers=1., timeout=1.0, check=5.,
     show = Show(name, cmd, num_workers, working_dir=working_dir,
                  warmup_delay=warmup_delay, shell=shell, uid=uid, gid=gid,
                  env=env)
-    return Trainer([show], controller, ipc_path=ipc_path)
+    return Trainer([show], controller)
