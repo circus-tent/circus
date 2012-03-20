0131ae0f50fc0b7dda52d825b38ffa3619e711bb
Tarek Ziade <tarek@ziade.org>
2012-03-20 21:09:26 +0100

    more metadata

--- setup.py
+++ setup.py
@@ -4,11 +4,14 @@ from setuptools import setup, find_packages
 setup(name='circus',
       version='0.1',
       packages=find_packages(),
+      author="Mozilla Foundation & contributors",
+      author_email="services-dev@lists.mozila.org",
       include_package_data=True,
       zip_safe=False,
       classifiers=[
         "Programming Language :: Python",
-        "License :: OSI Approved :: Apache Software License"],
+        "License :: OSI Approved :: Apache Software License",
+        "Development Status :: 3 - Alpha"],
       install_requires=['pyzmq', 'psutil', 'nose'],
       test_suite = 'nose.collector',
       entry_points="""
