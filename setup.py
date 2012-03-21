aecddba017fb8d1069883eb6487f01bf39f4f28b
Tarek Ziade <tarek@ziade.org>
2012-03-21 13:23:26 +0100

    added a CHANGES file and moved to 0.2 for the next release

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 from setuptools import setup, find_packages
 
+
 with open("README.rst") as f:
     README = f.read()
 
+with open("CHANGES.rst") as f:
+    CHANGES = f.read()
+
 
 setup(name='circus',
-      version='0.1',
+      version='0.2',
       packages=find_packages(),
       description=("Circus is a program that will let you run and watch "
                    " multiple processes."),
-      long_description=README,
+      long_description=README + '\n' + CHANGES,
       author="Mozilla Foundation & contributors",
       author_email="services-dev@lists.mozila.org",
       include_package_data=True,
