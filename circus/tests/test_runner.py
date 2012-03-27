6cbbbb5d28474c2746021957cf74122ba4aa293e
Tarek Ziade <tarek@ziade.org>
2012-03-27 22:16:35 +0200

    added a test for issue #53

--- circus/tests/test_runner.py
+++ circus/tests/test_runner.py
@@ -1,5 +1,13 @@
-from circus.tests.support import TestCircus
+import os
 import time
+import sys
+import threading
+import signal
+
+from circus.tests.support import TestCircus
+from circus.watcher import Watcher
+from circus.arbiter import Arbiter
+from circus.circusd import main
 
 
 def Dummy(test_file):
@@ -19,3 +27,18 @@ class TestRunner(TestCircus):
             content = f.read()
 
         self.assertTrue('.' in content)
+
+    def test_issue53(self):
+        watcher = Watcher('test', 'bash -q', numprocesses=10,
+                          warmup_delay=0)
+        endpoint = 'tcp://127.0.0.1:5555'
+        pubsub_endpoint = 'tcp://127.0.0.1:5556'
+        arbiter = Arbiter([watcher], endpoint, pubsub_endpoint, check_delay=0.1)
+
+        def handler(signum, frame):
+            arbiter.stop()
+
+        signal.signal(signal.SIGALRM, handler)
+        signal.alarm(10)
+
+        arbiter.start()
