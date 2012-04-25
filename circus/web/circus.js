938c365e48b644edfe54a6ba61d8a89e0e80fee1
Tarek Ziade <tarek@ziade.org>
2012-04-25 15:32:48 +0200

    now displaying the circusd mem/cpu usage

--- circus/web/circus.js
+++ circus/web/circus.js
@@ -7,7 +7,6 @@ var MAX_SIZE = 25;
 // then updating the graphs
 function updateGraphs() {
 
-
 $.each(cpu_data, function(key, val) {
   var id = '#' + key + '_cpu';
   var lastid = '#' + key + '_last_cpu';
@@ -65,5 +64,65 @@ function refreshData(name) {
     });
 
   updateGraphs();
+}
+
+
+/*
+* Circusd graph
+*/
+var circusd_cpu_data;
+var circusd_mem_data;
+
+function updateCircusdGraph() {
+  var id = '#circusd_cpu';
+  var lastid = '#circusd_last_cpu';
+  var last = circusd_cpu_data.length - 1;
+  $(lastid).text(circusd_cpu_data[last] + " %");
+  $(id).sparkline(circusd_cpu_data, {width: '55%'});
+  var id = '#circusd_mem';
+  var lastid = '#circusd_last_mem';
+  var last = circusd_mem_data.length - 1;
+  $(lastid).text(circusd_mem_data[last] + " %");
+  $(id).sparkline(circusd_mem_data, {width: '55%'});
+}
+
+function initializeCircusdGraph() {
+    $.getJSON('/circusd/stats/cpu', function(data) {
+        circusd_cpu_data = data['info'];
+    });
+    $.getJSON('/circusd/stats/mem', function(data) {
+        circusd_mem_data = data['info'];
+    });
+    updateCircusdGraph();
+}
+
+
+function refreshCircusdGraph() {
+
+    $.getJSON('/circusd/stats/cpu?start=-3&end=-1', function(data) {
+        var values = data['info'];
+        for (i=0;i<values.length;i++) {
+            circusd_cpu_data.push(values[i]);
+        }
+        if (circusd_cpu_data.length > MAX_SIZE) {
+              start = circusd_cpu_data.length - MAX_SIZE;
+              circusd_cpu_data = circusd_cpu_data.slice(start);
+        }
+     });
+
+    
+    $.getJSON('/circusd/stats/mem?start=-3&end=-1', function(data) {
+        var values = data['info'];
+
+        for (i=0;i<values.length;i++) {
+            circusd_mem_data.push(values[i]);
+        }
+        if (circusd_mem_data.length > MAX_SIZE) {
+              start = circusd_mem_data.length - MAX_SIZE;
+              circusd_mem_data = circusd_mem_data.slice(start);
+        }
+     });
+  
+  updateCircusdGraph();
 }
 
