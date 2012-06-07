c77ab3a4f8a3c816f2889c9556d946f31c8e06d6
Tarek Ziade <tarek@ziade.org>
2012-06-07 11:25:11 +0200

    improved coverage for circus.stats.streamer

--- circus/tests/test_stats_collector.py
+++ circus/tests/test_stats_collector.py
@@ -1,12 +1,17 @@
-import zmq
+import threading
+import json
 import unittest
 import time
 import os
 import Queue
 
+import zmq
+
 from circus.stats.collector import StatsCollector
+from circus.stats.streamer import StatsStreamer
 from circus.stats.publisher import StatsPublisher
-from circus.stats import publisher
+from circus.stats import publisher, streamer
+from circus import client
 
 
 class FakeStreamer(object):
@@ -26,11 +31,22 @@ class Socket(object):
     def __init__(self, *args, **kw):
         pass
 
-    closed = False
+    closed = bind_to_random_port = False
     data = []
+
+    def fileno(self):
+        return 0
+
+    def recv(self):
+        return json.dumps({'watchers': ['watcher'],
+                           'pids': [os.getpid()],
+                           'info': {'pid': os.getpid()}})
 
     def bind(self, *args):
         pass
+
+    connect = setsockopt = setsockopt_unicode = getsockopt = bind
+    send = getsockopt_unicode = bind
 
     def send_multipart(self, data):
         self.data.append(data)
@@ -43,21 +59,36 @@ class Context(object):
         pass
 
 
+class Poller(object):
+    def register(self, *args):
+        pass
+
+    def poll(self, *args):
+        return {Socket(): 'xx'}
+
+
 class FakeZMQ(object):
     Context = Context
-    PUB = None
+    DEALER = IDENTITY = LINGER = SUBSCRIBE = PUB = SUB = None
     ZMQError = zmq.ZMQError
+    Poller = Poller
+    POLLIN = None
 
 
 class TestStats(unittest.TestCase):
     def setUp(self):
         self._ctx = zmq.Context
         self._p = publisher.zmq
-        publisher.zmq = zmq.context = FakeZMQ
+        self._s = streamer.zmq
+        self._c = client.zmq
+        publisher.zmq = streamer.zmq = zmq.context = FakeZMQ
+        client.zmq = FakeZMQ
 
     def tearDown(self):
         zmq.context = self._ctx
         publisher.zmq = self._p
+        streamer.zmq = self._s
+        client.zmq = self._c
 
     def test_collector(self):
         streamer = FakeStreamer()
@@ -87,3 +118,24 @@ class TestStats(unittest.TestCase):
 
         topics = [data[0] for data in pub.socket.data]
         self.assertTrue('stat.watcher.%d' % os.getpid() in topics)
+
+    def test_streamer(self):
+
+        class ThreadedStream(threading.Thread):
+            def __init__(self):
+                threading.Thread.__init__(self)
+                self.daemon = True
+
+            def run(self):
+                self.s = streamer = StatsStreamer('endpoint', 'pub', 'stats')
+                streamer.start()
+
+            def stop(self):
+                self.s.stop()
+
+        th = ThreadedStream()
+        th.start()
+        time.sleep(1.)
+        th.stop()
+
+        self.assertTrue(len(th.s.publisher.socket.data) > 3)
