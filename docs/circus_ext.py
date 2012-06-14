ce3eb3152e7f820b18a3f3fe6f5e729d3e723de8
Alexis Metaireau <alexis@notmyidea.org>
2012-06-14 16:45:28 +0200

    Put the commands in a separate documentation page.

--- docs/circus_ext.py
+++ docs/circus_ext.py
@@ -2,61 +2,20 @@ import os
 from circus.commands import get_commands
 
 _HEADER = """\
-.. _cli:
 
-==================
-Command-line tools
-==================
+.. _commands:
 
-circus-top
-==========
+Commands
+########
 
-*circus-top* is a top-like console you can run to watch
-live your running Circus system. It will display the CPU and Memory
-usage.
-
-Example of output::
-
-    -----------------------------------------------------------------------
-    circusd-stats
-     PID                 CPU (%)             MEMORY (%)
-    14252                 0.8                 0.4
-                          0.8 (avg)           0.4 (sum)
-
-    dummy
-     PID                 CPU (%)             MEMORY (%)
-    14257                 78.6                0.1
-    14256                 76.6                0.1
-    14258                 74.3                0.1
-    14260                 71.4                0.1
-    14259                 70.7                0.1
-                          74.32 (avg)         0.5 (sum)
-
-    ----------------------------------------------------------------------
-
-
-
-*circus-top* is a read-only console. If you want to interact with the system, use
-*circusctl*.
-
-
-circusctl
-=========
-
-*circusctl* can be used to run any command listed below. For
-example, you can get a list of all the watchers::
-
-    $ circusctl list
-
-
-*circusctl* is just a zeromq client, and if needed you can
-drive programmaticaly the Circus system by writing your
-own zmq client.
+At the epicenter of circus lives the command systems.  *circusctl* is just a
+zeromq client, and if needed you can drive programmaticaly the Circus system by
+writing your own zmq client.
 
 All messages are Json mappings.
 
-For each command below, we provide a usage example with
-circusctl but also the input / output zmq messages.
+For each command below, we provide a usage example with circusctl but also the
+input / output zmq messages.
 
 """
 
