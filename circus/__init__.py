d879fb113d0156a196b34f1d0921cecf0940247c
Tarek Ziade <tarek@ziade.org>
2012-06-25 22:00:11 +0200

    we missed a patch here

--- circus/__init__.py
+++ circus/__init__.py
@@ -4,9 +4,10 @@ import sys
 
 
 try:
-    import gevent       # NOQA
+    from gevent import monkey       # NOQA
     try:
         from gevent_zeromq import monkey_patch, IOLOOP_IS_MONKEYPATCHED  # NOQA
+        monkey()
         monkey_patch()
     except ImportError:
         msg = """We have detected that you have gevent in your
