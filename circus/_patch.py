00fd2f621548fba8de338b3ea32b6557b1ef61e2
Tarek Ziade <tarek@ziade.org>
2012-07-12 09:45:57 +0200

    patching the threading module for the KeyError - relates #203

---
+++ circus/_patch.py
@@ -0,0 +1,69 @@
+import threading
+from threading import (_active_limbo_lock, _limbo, _active, _sys, _trace_hook,
+        _profile_hook, _format_exc)
+
+
+def _bootstrap_inner(self):
+    try:
+        self._set_ident()
+        self._started.set()
+        with _active_limbo_lock:
+            _active[self._Thread__ident] = self
+            del _limbo[self]
+
+        if _trace_hook:
+            _sys.settrace(_trace_hook)
+        if _profile_hook:
+            _sys.setprofile(_profile_hook)
+
+        try:
+            self.run()
+        except SystemExit:
+            pass
+        except:
+            if _sys:
+                _sys.stderr.write("Exception in thread %s:\n%s\n" %
+                                    (self.name, _format_exc()))
+            else:
+                exc_type, exc_value, exc_tb = self._exc_info()
+                try:
+                    self._stderr.write(
+                        (
+                        "Exception in thread " + self.name +
+                        " (most likely raised during interpreter shutdown):")
+                        )
+
+                    self._stderr.write("Traceback (most recent call last):")
+                    while exc_tb:
+                        self._stderr.write(
+                            '  File "%s", line %s, in %s' %
+                            (exc_tb.tb_frame.f_code.co_filename,
+                                exc_tb.tb_lineno,
+                                exc_tb.tb_frame.f_code.co_name))
+
+                        exc_tb = exc_tb.tb_next
+                    self._stderr.write("%s: %s" % (exc_type, exc_value))
+                finally:
+                    del exc_type, exc_value, exc_tb
+        finally:
+            pass
+    finally:
+        with _active_limbo_lock:
+            self._stop()
+            try:
+                del _active[self._Thread__ident]
+            except:
+                pass
+
+
+def _delete(self):
+    try:
+        with _active_limbo_lock:
+            del _active[self._Thread__ident]
+    except KeyError:
+        if 'dummy_threading' not in _sys.modules:
+            raise
+
+
+threading.Thread._Thread__bootstrap_inner = _bootstrap_inner
+threading.Thread._Thread__delete = _delete
