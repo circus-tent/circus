833d8de263766279f734afaeb66735bf794c8471
Alexis Metaireau <alexis@notmyidea.org>
2012-07-17 16:51:55 +0200

    update the 'run' command to use the properties facility

--- circus/commands/run.py
+++ circus/commands/run.py
@@ -7,7 +7,8 @@ class Run(Command):
         Run a watcher
         ==============================
 
-        This command runs the process in a watcher
+        This command runs one watcher's process and does not try to control it
+        in any way: it's a fire and forget thing.
 
 
         ZMQ Message
@@ -40,14 +41,8 @@ class Run(Command):
 
     """
     name = "run"
+    properties = ['name', ]
 
-    def message(self, *args, **opts):
-        if len(args) != 1:
-            raise ArgumentError("invalid number of arguments")
-
-        return self.make_message(name=args[0])
-    
     def execute(self, arbiter, props):
-        if 'name' in props:
-            watcher = self._get_watcher(arbiter, props['name'])
-            watcher.run()
+        watcher = self._get_watcher(arbiter, props['name'])
+        watcher.run()
