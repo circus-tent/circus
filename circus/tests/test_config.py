b0ce6046f13cfb0da21dc3ebdf75ed1fe68a6cc6
Nathan Cahill <nathan@nathancahill.com>
2012-09-29 13:35:34 -0400

    Added test for env variables

--- circus/tests/test_config.py
+++ circus/tests/test_config.py
@@ -9,6 +9,7 @@ _CONF = {
     'issue137': os.path.join(HERE, 'issue137.ini'),
     'include': os.path.join(HERE, 'include.ini'),
     'issue210': os.path.join(HERE, 'issue210.ini'),
+    'env_var': os.path.join(HERE, 'env_var.ini'),
 }
 
 
@@ -22,9 +23,16 @@ class TestConfig(unittest.TestCase):
     def test_include_wildcards(self):
         conf = get_config(_CONF['include'])
         watchers = conf['watchers']
-        self.assertEquals(len(watchers), 4)
+        self.assertEquals(len(watchers), 2)
 
     def test_watcher_graceful_timeout(self):
         conf = get_config(_CONF['issue210'])
         watcher = Watcher.load_from_config(conf['watchers'][0])
         watcher.stop()
+
+    def test_watcher_env_var(self):
+        conf = get_config(_CONF['env_var'])
+        watcher = Watcher.load_from_config(conf['watchers'][0])
+        self.assertEquals("%s:/bin" % os.getenv('PATH'), watcher.env['PATH'])
+        watcher.stop()
+
