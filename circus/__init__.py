c9e2e95acfbf15e7e7362eca02b31fc17981797a
Eric Larson <eric@ionrock.org>
2012-12-31 00:58:03 -0600

    Slight cleanup in the get_arbiter docstring to avoid autodoc errors

--- circus/__init__.py
+++ circus/__init__.py
@@ -21,9 +21,8 @@ def get_arbiter(watchers, controller=None,
 
     Options:
 
-    - **watchers** -- a list of watchers. A watcher in that case is a
-      dict containing:
-
+      - **watchers** -- a list of watchers. A watcher in that case is a
+                        dict containing:
         - **name** -- the name of the watcher (default: None)
         - **cmd** -- the command line used to run the Watcher.
         - **args** -- the args for the command (list or string).
@@ -48,9 +47,9 @@ def get_arbiter(watchers, controller=None,
           the stdout stream. Default to None. When provided, may contain:
 
             - **class**: the fully qualified name of the class to use for
-              streaming. Defaults to circus.stream.FileStream
+                         streaming. Defaults to circus.stream.FileStream
             - **refresh_time**: the delay between two stream checks. Defaults
-              to 0.3 seconds.
+                                to 0.3 seconds.
             - any other key will be passed the class constructor.
         - **stderr_stream**: a mapping containing the options for configuring
           the stderr stream. Default to None. When provided, may contain:
@@ -58,7 +57,7 @@ def get_arbiter(watchers, controller=None,
             - **class**: the fully qualified name of the class to use for
               streaming. Defaults to circus.stream.FileStream
             - **refresh_time**: the delay between two stream checks. Defaults
-              to 0.3 seconds.
+                                to 0.3 seconds.
             - any other key will be passed the class constructor.
         - **max_retry**: the number of times we attempt to start a process,
           before we abandon and stop the whole watcher. (default: 5)
