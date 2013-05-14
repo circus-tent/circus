8d09ba9364f2b158785c47055f518ba9d18a181f
Marcus Brinkmann <marcus.brinkmann@ruhr-uni-bochum.de>
2013-05-14 12:38:10 +0200

    Integrate log handlers into zmq io loop.
    Currently, circus polls logging handlers once every second.  This is inefficient,
    and easily stalls processes that emit more log messages in one second than fits
    into the systems pipe buffer (typically 4-64 KB).  I have actually seen this
    happening with BrightcoveOS's Diamond server.
    
    This change integrates log handling into the zmq ioloop, which should handle any
    amount of log traffic fairly and without arbitrary delay.

--- circus/stream/redirector.py
+++ circus/stream/redirector.py
@@ -5,24 +5,6 @@ import sys
 
 from zmq.eventloop import ioloop
 
-
-class NamedPipe(object):
-    def __init__(self, pipe, process, name):
-        self.pipe = pipe
-        self.process = process
-        self.name = name
-        fcntl.fcntl(pipe, fcntl.F_SETFL, os.O_NONBLOCK)
-        self._fileno = pipe.fileno()
-
-    def fileno(self):
-        return self._fileno
-
-    def read(self, buffer):
-        if self.pipe.closed:
-            return
-        return self.pipe.read(buffer)
-
-
 class Redirector(object):
     def __init__(self, redirect, refresh_time=1.0, extra_info=None,
                  buffer=1024, loop=None):
@@ -31,52 +13,45 @@ class Redirector(object):
         self.redirect = redirect
         self.extra_info = extra_info
         self.buffer = buffer
-        self.running = False
         if extra_info is None:
             extra_info = {}
         self.extra_info = extra_info
-        self.refresh_time = refresh_time * 1000
         self.loop = loop or ioloop.IOLoop.instance()
-        self.caller = None
 
     def start(self):
-        self.caller = ioloop.PeriodicCallback(self._select, self.refresh_time,
-                                              self.loop)
-        self.caller.start()
+        pass
 
     def kill(self):
-        if self.caller is None:
-            return
-        self.caller.stop()
+        for key, pipe in self._names.items():
+            self.loop.remove_handler(pipe.fileno())
+            del self._names[key]
 
     def add_redirection(self, name, process, pipe):
-        npipe = NamedPipe(pipe, process, name)
-        self.pipes.append(npipe)
-        self._names[process.pid, name] = npipe
+        def _handler(fd, events):
+            if not (events & self.loop.READ):
+                return
+            try:
+                data = os.read(fd, self.buffer)
+                if len(data) == 0:
+                    self.remove_redirection(name, process)
+                else:
+                    datamap = {'data': data, 'pid': process.pid,
+                               'name': name}
+                    datamap.update(self.extra_info)
+                    self.redirect(datamap)
+            except IOError, ex:
+                if ex[0] != errno.EAGAIN:
+                    raise
+                sys.exc_clear()
+
+        self.loop.add_handler(pipe.fileno(), _handler, self.loop.READ)
+        self._names[process.pid, name] = pipe
 
     def remove_redirection(self, name, process):
         key = process.pid, name
         if key not in self._names:
             return
         pipe = self._names[key]
-        self.pipes.remove(pipe)
+        self.loop.remove_handler(pipe.fileno())
         del self._names[key]
 
-    def _select(self):
-        if len(self.pipes) == 0:
-            return
-
-        # we just try to read, if we see some data
-        # we just redirect it.
-        try:
-            for pipe in self.pipes:
-                data = pipe.read(self.buffer)
-                if data:
-                    datamap = {'data': data, 'pid': pipe.process.pid,
-                               'name': pipe.name}
-                    datamap.update(self.extra_info)
-                    self.redirect(datamap)
-        except IOError, ex:
-            if ex[0] != errno.EAGAIN:
-                raise
-            sys.exc_clear()
