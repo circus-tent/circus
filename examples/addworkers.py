5f588e0ad6b72d10500e2d5da2ce24ece0db4d14
Tarek Ziade <tarek@ziade.org>
2013-05-16 14:17:37 +0200

    Added an example for #386

---
+++ examples/addworkers.py
@@ -0,0 +1,32 @@
+import sys
+
+from circus.client import CircusClient
+from circus.util import DEFAULT_ENDPOINT_DEALER
+
+client = CircusClient(endpoint=DEFAULT_ENDPOINT_DEALER)
+
+command = '../bin/python dummy_fly.py 111'
+name = 'dummy'
+
+
+for i in range(50):
+    print client.call("""
+    {
+        "command": "add",
+        "properties": {
+            "cmd": "%s",
+            "name": "%s",
+            "options": {
+            "copy_env": true,
+            "stdout_stream": {
+                "filename": "stdout.log"
+            },
+            "stderr_stream": {
+                "filename": "stderr.log"
+            }
+            },
+            "start": true
+        }
+    }
+    """ % (command, name + str(i)))
+
