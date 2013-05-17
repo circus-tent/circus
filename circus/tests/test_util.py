bf34e1d251dfcf3b2fe67dc76ea94d5289414232
Francisco Souza <f@souza.cc>
2013-05-17 19:32:48 -0300

    util: fix bug in to_uid function
    It must check for string_types, not str only.

--- circus/tests/test_util.py
+++ circus/tests/test_util.py
@@ -3,6 +3,7 @@ import os
 import pwd
 import unittest
 from psutil import Popen
+from mock import Mock, patch
 
 from circus.util import (get_info, bytes2human, to_bool, parse_env_str,
                          env_to_str, to_uid, to_gid, replace_gnu_args,
@@ -13,7 +14,7 @@ class TestUtil(unittest.TestCase):
 
     def test_get_info(self):
 
-        worker = Popen(["python -c 'import time;time.sleep(5)'"], shell=True)
+        worker = Popen(['python -c 'import time;time.sleep(5)''], shell=True)
         try:
             info = get_info(worker)
         finally:
@@ -42,6 +43,16 @@ class TestUtil(unittest.TestCase):
         parsed = parse_env_str(env)
         self.assertEqual(parsed, {'test': '1', 'booo': '2'})
         self.assertEqual(env_to_str(parsed), env)
+
+    def test_to_uid(self):
+        with patch('pwd.getpwnam') as getpw:
+            m = Mock()
+            m.pw_uid = '1000'
+            getpw.return_value = m
+            uid = to_uid(u'user')
+            self.assertEqual('1000', uid)
+            uid = to_uid('user')
+            self.assertEqual('1000', uid)
 
     def test_to_uidgid(self):
         self.assertRaises(ValueError, to_uid, 'xxxxxxx')
