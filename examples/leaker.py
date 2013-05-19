109de9fdd98d81d2d6d85ded4b2e38c9ba8d024f
Tarek Ziade <tarek@ziade.org>
2013-05-19 23:33:44 +0200

    make sure the leak happens only when the program is launched, not loaded as a module

--- examples/leaker.py
+++ examples/leaker.py
@@ -1,8 +1,9 @@
 # sleeps for 55555 then leaks memory
 import time
 
-time.sleep(5)
-memory = ''
+if __name__ == '__main__':
+    time.sleep(5)
+    memory = ''
 
-while True:
-    memory += 100000 * ' '
+    while True:
+        memory += 100000 * ' '
