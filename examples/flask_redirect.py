006ca9388be6cf4828f5786edd87763c6b2e763e
Tarek Ziade <tarek@ziade.org>
2013-05-20 18:11:44 +0200

    example for #272

---
+++ examples/flask_redirect.py
@@ -0,0 +1,18 @@
+from flask import Flask, redirect, make_response
+app = Flask(__name__)
+app.debug = True
+
+@app.route("/file.pdf")
+def file():
+    with open('file.pdf', 'rb') as f:
+        response = make_response(f.read())
+        response.headers['Content-Type'] = "application/pdf"
+        return response
+
+@app.route("/")
+def page_redirect():
+    return redirect("http://localhost:8000")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8181, host='0.0.0.0')
---
+++ examples/flask_serve.py
@@ -0,0 +1,15 @@
+from flask import Flask, redirect, make_response
+app = Flask(__name__)
+app.debug = True
+import requests
+
+@app.route("/")
+def pdf():
+    pdf = requests.get('http://localhost:5000/file.pdf')
+    response = make_response(pdf.content)
+    response.headers['Content-Type'] = "application/pdf"
+    return response
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8181, host='0.0.0.0')
