c4455519e672c8264d8b67b9ce172436f9db083f
Tarek Ziade <tarek@ziade.org>
2013-06-06 22:46:50 +0200

    small api example

---
+++ examples/apis.py
@@ -0,0 +1,12 @@
+
+from circus import get_arbiter
+
+myprogram = {"cmd": "sleep 30", "numprocesses": 4}
+
+print 'Runnning...'
+arbiter = get_arbiter([myprogram])
+try:
+    arbiter.start()
+finally:
+    arbiter.stop()
+
