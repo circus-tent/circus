4130f8b30a5dc04e0393591f135f9747c0f8a618
Tarek Ziade <tarek@ziade.org>
2013-06-11 10:54:36 +0200

    added the test

---
+++ circus/tests/test_circusd.py
@@ -0,0 +1,26 @@
+import unittest
+import sys
+
+from circus.circusd import daemonize
+
+
+class TestCircusd(unittest.TestCase):
+
+    def setUp(self):
+        self.saved = dict(sys.modules)
+
+    def tearDown(self):
+        sys.modules = self.saved
+
+    def test_daemon(self):
+        # if gevent is loaded, we want to prevent
+        # daemonize() to work
+        import gevent       # NOQA
+        self.assertRaises(ValueError, daemonize)
+
+        for module in sys.modules.keys():
+            if module.startswith('gevent'):
+                del sys.modules[module]
+
+        from gevent.dns import resolve_ipv4     # NOQA
+        self.assertRaises(ValueError, daemonize)
