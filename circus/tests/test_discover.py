e31f649d93e69749fc1e34441db9b1024784838a
Alexis Métaireau <alexis@notmyidea.org>
2013-07-09 14:08:39 +0200

    fix the autodiscovery test

--- circus/tests/test_discover.py
+++ circus/tests/test_discover.py
@@ -11,15 +11,14 @@ class TestDiscover(unittest.TestCase):
         payload = "Beer. Now there's a temporary solution."
         loop = ioloop.IOLoop.instance()
 
-        received_data = None
+        self.received_data = None
 
         def cb(data, emitter_addr, send_message):
-            global received_data
-            received_data = data
+            self.received_data = data
             loop.stop()
 
         AutoDiscovery('udp://237.219.251.97:12027', loop, payload, cb)
         loop.add_timeout(loop.time() + 1, loop.stop)
 
         loop.start()
-        self.assertEquals(received_data, payload)
+        self.assertEquals(self.received_data['nodes'], payload)
