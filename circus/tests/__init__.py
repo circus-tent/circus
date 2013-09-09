7c2749a5de930c23c8940091cec972609b304bc1
Tarek Ziade <tarek@ziade.org>
2013-09-09 13:30:36 +0200

    silent logger during tests - fixes #521

--- circus/tests/__init__.py
+++ circus/tests/__init__.py
@@ -1,3 +1,13 @@
+import os
+from circus.util import configure_logger
+from circus import logger
+
+
+_CONFIGURED = False
+
+if not _CONFIGURED and 'TESTING' in os.environ:
+    configure_logger(logger, level='CRITICAL')
+    _CONFIGURED = True
 
 
 def setUp():
