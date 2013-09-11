628529bcf17f600fe947a4eebba086641842bead
Alex Marandon <alex.marandon@makina-corpus.com>
2013-09-11 14:02:12 +0200

    Fix lines too long

--- circus/tests/test_reloadconfig.py
+++ circus/tests/test_reloadconfig.py
@@ -12,14 +12,18 @@ _CONF = {
     'reload_numprocesses': os.path.join(CONFIG_DIR, 'reload_numprocesses.ini'),
     'reload_addwatchers': os.path.join(CONFIG_DIR, 'reload_addwatchers.ini'),
     'reload_delwatchers': os.path.join(CONFIG_DIR, 'reload_delwatchers.ini'),
-    'reload_changewatchers': os.path.join(CONFIG_DIR, 'reload_changewatchers.ini'),
+    'reload_changewatchers': os.path.join(CONFIG_DIR,
+                                          'reload_changewatchers.ini'),
     'reload_addplugins': os.path.join(CONFIG_DIR, 'reload_addplugins.ini'),
     'reload_delplugins': os.path.join(CONFIG_DIR, 'reload_delplugins.ini'),
-    'reload_changeplugins': os.path.join(CONFIG_DIR, 'reload_changeplugins.ini'),
+    'reload_changeplugins': os.path.join(CONFIG_DIR,
+                                         'reload_changeplugins.ini'),
     'reload_addsockets': os.path.join(CONFIG_DIR, 'reload_addsockets.ini'),
     'reload_delsockets': os.path.join(CONFIG_DIR, 'reload_delsockets.ini'),
-    'reload_changesockets': os.path.join(CONFIG_DIR, 'reload_changesockets.ini'),
-    'reload_changearbiter': os.path.join(CONFIG_DIR, 'reload_changearbiter.ini'),
+    'reload_changesockets': os.path.join(CONFIG_DIR,
+                                         'reload_changesockets.ini'),
+    'reload_changearbiter': os.path.join(CONFIG_DIR,
+                                         'reload_changearbiter.ini'),
 }
 
 
