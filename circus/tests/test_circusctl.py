c70ce9929c0f73757bdc4a129894f196f8d1021c
Alex Marandon <alex.marandon@makina-corpus.com>
2013-10-02 17:35:44 +0200

    Convert test_circusctl.py to asynchronous style

--- circus/tests/test_circusctl.py
+++ circus/tests/test_circusctl.py
@@ -1,12 +1,17 @@
 import subprocess
 import sys
 import shlex
+from multiprocessing import Process, Queue
+
+from tornado.testing import gen_test
+from tornado.gen import coroutine, Return
 
 from circus.circusctl import USAGE, VERSION, CircusCtl
 from circus.tests.support import TestCircus, poll_for
+from circus.util import tornado_sleep
 
 
-def run_ctl(args, stdin=''):
+def run_ctl(args, queue=None, stdin=''):
     cmd = '%s -m circus.circusctl' % sys.executable
     proc = subprocess.Popen(cmd.split() + shlex.split(args),
                             stdin=subprocess.PIPE,
@@ -17,6 +22,9 @@ def run_ctl(args, stdin=''):
 
     stderr = proc.stderr.read()
     stdout = proc.stdout.read()
+    if queue:
+        queue.put(stderr)
+        queue.put(stdout)
     proc.wait()
     try:
         import gevent
@@ -26,11 +34,25 @@ def run_ctl(args, stdin=''):
     return stdout, stderr
 
 
+@coroutine
+def async_run_ctl(args, stdin=''):
+    """
+    Start a process that will start the actual circusctl process and poll its
+    ouput, via a queue, without blocking the I/O loop. We do this to avoid
+    blocking the main thread while waiting for circusctl output, so that the
+    arbiter will be able to respond to requests coming from circusctl.
+    """
+    queue = Queue()
+    circusctl_process = Process(target=run_ctl, args=(args, queue, stdin))
+    circusctl_process.start()
+    while queue.empty():
+        yield tornado_sleep(.1)
+    stderr = queue.get()
+    stdout = queue.get()
+    raise Return((stdout, stderr))
+
+
 class CommandlineTest(TestCircus):
-    def setUp(self):
-        super(CommandlineTest, self).setUp()
-        test_file = self._run_circus('circus.tests.support.run_process')
-        poll_for(test_file, 'START')
 
     def test_help_switch_no_command(self):
         stdout, stderr = run_ctl('--help')
@@ -53,38 +75,48 @@ class CommandlineTest(TestCircus):
         self.assertEqual(stderr, '')
         self.assertEqual(stdout.splitlines()[0], 'Add a watcher')
 
+    @gen_test
     def test_add(self):
-        stdout, stderr = run_ctl('add test2 "sleep 1"')
+        yield self.start_arbiter()
+        poll_for(self.test_file, 'START')
+
+        stdout, stderr = yield async_run_ctl('add test2 "sleep 1"')
         self.assertEqual(stderr, '')
         self.assertEqual(stdout, 'ok\n')
-        stdout, stderr = run_ctl('status test2')
+
+        stdout, stderr = yield async_run_ctl('status test2')
         self.assertEqual(stderr, '')
         self.assertEqual(stdout, 'stopped\n')
+        yield self.stop_arbiter()
 
+    @gen_test
     def test_add_start(self):
-        stdout, stderr = run_ctl('add --start test2 "sleep 1"')
+        yield self.start_arbiter()
+        stdout, stderr = yield async_run_ctl('add --start test2 "sleep 1"')
         self.assertEqual(stderr, '')
         self.assertEqual(stdout, 'ok\n')
-        stdout, stderr = run_ctl('status test2')
+        stdout, stderr = yield async_run_ctl('status test2')
         self.assertEqual(stderr, '')
         self.assertEqual(stdout, 'active\n')
+        yield self.stop_arbiter()
 
 
 class CLITest(TestCircus):
 
-    def setUp(self):
-        super(CLITest, self).setUp()
-        test_file = self._run_circus('circus.tests.support.run_process')
-        poll_for(test_file, 'START')
-
+    @coroutine
     def run_ctl(self, command=''):
         """Send the given command to the CLI, and ends with EOF."""
         if command:
             command += '\n'
-        return run_ctl('', command + 'EOF\n')
+        stdout, stderr = yield async_run_ctl('', command + 'EOF\n')
+        raise Return((stdout, stderr))
 
+    @gen_test
     def test_launch_cli(self):
-        stdout, stderr = self.run_ctl()
+        yield self.start_arbiter()
+        poll_for(self.test_file, 'START')
+
+        stdout, stderr = yield self.run_ctl()
         self.assertEqual(stderr, '')
         output = stdout.splitlines()
         self.assertEqual(output[0], VERSION)
@@ -92,8 +124,10 @@ class CLITest(TestCircus):
         prompt = output[2][-len(CircusCtl.prompt):]
         self.assertEqual(prompt, CircusCtl.prompt)
 
+        yield self.stop_arbiter()
+
     def test_cli_help(self):
-        stdout, stderr = self.run_ctl('help')
+        stdout, stderr = yield self.run_ctl('help')
         self.assertEqual(stderr, '')
         prompt = stdout.splitlines()
         # first two lines are VERSION and prompt, followed by a blank line
