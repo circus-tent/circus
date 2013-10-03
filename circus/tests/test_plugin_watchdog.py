040899917654afe97b238ead08c1a19cf47fff44
Alex Marandon <alex.marandon@makina-corpus.com>
2013-10-03 16:09:39 +0200

    Fix watchdog plugin test

--- circus/tests/test_plugin_watchdog.py
+++ circus/tests/test_plugin_watchdog.py
@@ -2,13 +2,18 @@ import socket
 import time
 import os
 import warnings
+import multiprocessing
+
+from tornado.gen import Return, coroutine
+from tornado.testing import gen_test
 
 from circus.tests.support import TestCircus, Process, poll_for
+from circus.util import tornado_sleep
 from circus.util import (DEFAULT_ENDPOINT_DEALER, DEFAULT_ENDPOINT_SUB)
 from circus.plugins.watchdog import WatchDog
 
 
-def run_plugin(klass, config, duration=300):
+def run_plugin(klass, config, queue, duration=300):
     endpoint = DEFAULT_ENDPOINT_DEALER
     pubsub_endpoint = DEFAULT_ENDPOINT_SUB
     check_delay = 1
@@ -20,7 +25,20 @@ def run_plugin(klass, config, duration=300):
     deadline = time.time() + (duration / 1000.)
     plugin.loop.add_timeout(deadline, plugin.stop)
     plugin.start()
+    queue.put(plugin.pid_status)
     return plugin
+
+
+@coroutine
+def async_run_plugin(klass, config):
+    queue = multiprocessing.Queue()
+    circusctl_process = multiprocessing.Process(target=run_plugin,
+                                                args=(klass, config, queue))
+    circusctl_process.start()
+    while queue.empty():
+        yield tornado_sleep(.1)
+    pid_status = queue.get()
+    raise Return(pid_status)
 
 
 class DummyWatchDogged(Process):
@@ -48,20 +66,25 @@ fqn = 'circus.tests.test_plugin_watchdog.run_dummy_watchdogged'
 
 
 class TestPluginWatchDog(TestCircus):
-    def setUp(self):
-        super(TestPluginWatchDog, self).setUp()
-        self.test_file = self._run_circus(fqn)
+
+    @gen_test
+    def test_watchdog_discovery_found(self):
+        yield self.start_arbiter(fqn)
         poll_for(self.test_file, 'START')
 
-    def test_watchdog_discovery_found(self):
         config = {'loop_rate': 0.1, 'watchers_regex': "^test.*$"}
         with warnings.catch_warnings():
-            watchdog = run_plugin(WatchDog, config)
-        time.sleep(.4)  # ensure at least one loop in plugin
-        self.assertEqual(len(watchdog.pid_status), 1, watchdog.pid_status)
+            pid_status = yield async_run_plugin(WatchDog, config)
+        self.assertEqual(len(pid_status), 1, pid_status)
+        yield self.stop_arbiter()
 
+    @gen_test
     def test_watchdog_discovery_not_found(self):
-        config = {'loop_rate': 0.3, 'watchers_regex': "^foo.*$"}
-        watchdog = run_plugin(WatchDog, config)
-        time.sleep(.4)  # ensure at least one loop in plugin
-        self.assertEqual(len(watchdog.pid_status), 0)
+        yield self.start_arbiter(fqn)
+        poll_for(self.test_file, 'START')
+
+        config = {'loop_rate': 0.1, 'watchers_regex': "^foo.*$"}
+        with warnings.catch_warnings():
+            pid_status = yield async_run_plugin(WatchDog, config)
+        self.assertEqual(len(pid_status), 0, pid_status)
+        yield self.stop_arbiter()
