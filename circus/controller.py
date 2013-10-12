a00e962bcc6754de1a608911258b3126001f5912
Scott Maxwell <scott@codecobblers.com>
2013-10-11 18:22:03 -0700

    Close UDP socket to prevent ResourceWarning

--- circus/controller.py
+++ circus/controller.py
@@ -32,6 +32,8 @@ class Controller(object):
         self.context = context
         self.loop = loop
         self.check_delay = check_delay * 1000
+        self.ctrl_socket = None
+        self.udp_socket = None
         self.started = False
 
         # initialize the sys handler
@@ -74,6 +76,7 @@ class Controller(object):
 
     def stop(self):
         if self.started:
+            self.started = False
             self.caller.stop()
             try:
                 self.stream.flush()
@@ -82,6 +85,8 @@ class Controller(object):
                 pass
             self.ctrl_socket.close()
         self.sys_hdl.stop()
+        if self.udp_socket:
+            self.udp_socket.close()
 
     def handle_message(self, raw_msg):
         cid, msg = raw_msg
@@ -98,10 +103,11 @@ class Controller(object):
         self.loop.add_callback(functools.partial(self.dispatch, (cid, msg)))
 
     def handle_autodiscover_message(self, fd_no, type):
-        data, address = self.udp_socket.recvfrom(1024)
-        data = json.loads(data)
-        self.udp_socket.sendto(json.dumps({'endpoint': self.endpoint}),
-                               address)
+        if self.started:
+            data, address = self.udp_socket.recvfrom(1024)
+            data = json.loads(data)
+            self.udp_socket.sendto(json.dumps({'endpoint': self.endpoint}),
+                                   address)
 
     def dispatch(self, job):
         cid, msg = job
