f1ea58653621977b2d60af8ed8f87266b2b1d8b9
Scott Maxwell <scott@codecobblers.com>
2013-10-24 08:13:59 -0700

    Changes for Python 3.2 support

--- examples/apis.py
+++ examples/apis.py
@@ -3,7 +3,7 @@ from circus import get_arbiter
 
 myprogram = {"cmd": "sleep 30", "numprocesses": 4}
 
-print 'Runnning...'
+print('Runnning...')
 arbiter = get_arbiter([myprogram])
 try:
     arbiter.start()
