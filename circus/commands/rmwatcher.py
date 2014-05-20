be9599303370a506595327036c264a2c546c1b22
Wynn Wilkes <wynnwilkes@gmail.com>
2014-05-20 09:33:56 -0600

    Add a --nostop option to the rm command
    - This supports use cases for managing dynamically added processes
    which run for a unknown amount of time. The processes can be managed
    and respawned while they are running. However, when the processes
    know they are done they should not be respawned anymore. The
    processes (which are the only ones that know when they should be
    completely stopped) can then tell circusd to remove them but not try
    to kill them. The processes can then manage their own exit and not
    have to worry about race conditions and syncronicity issues inherent
    in trying to send a message to circusd which involves circusd trying
    to kill the client sending the message.
    
    - add a test for the nostop functionality

--- circus/commands/rmwatcher.py
+++ circus/commands/rmwatcher.py
@@ -7,8 +7,8 @@ class RmWatcher(Command):
         Remove a watcher
         ================
 
-        This command remove a watcher dynamically from the arbiter. The
-        watchers are gracefully stopped.
+        This command removes a watcher dynamically from the arbiter. The
+        watchers are gracefully stopped by default.
 
         ZMQ Message
         -----------
@@ -19,37 +19,47 @@ class RmWatcher(Command):
                 "command": "rm",
                 "properties": {
                     "name": "<nameofwatcher>",
+                    "nostop": False,
                     "waiting": False
                 }
             }
 
         The response return a status "ok".
 
-        If ``waiting`` is False (default), the call will return immediatly
+        If ``nostop`` is True (default: False), the processes for the watcher
+        will not be stopped - instead the watcher will just be forgotten by
+        circus and the watcher processes will be responsible for stopping
+        themselves. If ``nostop`` is not specified or is False, then the
+        watcher processes will be stopped gracefully.
+
+        If ``waiting`` is False (default), the call will return immediately
         after starting to remove and stop the corresponding watcher.
 
         If ``waiting`` is True, the call will return only when the remove and
-        stop process is completly ended. Because of the
+        stop process is completely ended. Because of the
         :ref:`graceful_timeout option <graceful_timeout>`, it can take some
         time.
+
 
         Command line
         ------------
 
         ::
 
-            $ circusctl rm <name> [--waiting]
+            $ circusctl rm <name> [--waiting] [--nostop]
 
         Options
         +++++++
 
         - <name>: name of the watcher to remove
+        - nostop: do not stop the watcher processes, just remove the watcher
 
     """
 
     name = "rm"
     properties = ['name']
-    options = Command.waiting_options
+    options = Command.waiting_options + \
+        [('nostop', 'nostop', False, 'Do not stop watcher processes')]
 
     def message(self, *args, **opts):
         if len(args) < 1 or len(args) > 1:
@@ -59,4 +69,4 @@ class RmWatcher(Command):
 
     def execute(self, arbiter, props):
         self._get_watcher(arbiter, props['name'])
-        return arbiter.rm_watcher(props['name'])
+        return arbiter.rm_watcher(props['name'], props.get('nostop', False))
