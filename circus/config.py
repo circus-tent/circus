a17c7d1a1f1ec7ca0be0f5e4c637257afd5c7f9e
Marek Majkowski <marek@cloudflare.com>
2014-09-18 21:37:30 +0100

    Cosmetic: if one section name matches, don't check others
    And also, warn when we read bad section

--- circus/config.py
+++ circus/config.py
@@ -183,14 +183,14 @@ def get_config(config_file):
             sock['replace'] = dget(section, "replace", False, bool)
             sockets.append(sock)
 
-        if section.startswith("plugin:"):
+        elif section.startswith("plugin:"):
             plugin = dict(cfg.items(section))
             plugin['name'] = section
             if 'priority' in plugin:
                 plugin['priority'] = int(plugin['priority'])
             plugins.append(plugin)
 
-        if section.startswith("watcher:"):
+        elif section.startswith("watcher:"):
             watcher = watcher_defaults()
             watcher['name'] = section.split("watcher:", 1)[1]
 
@@ -250,6 +250,8 @@ def get_config(config_file):
                 watcher['env'] = dict(local_env)
 
             watchers.append(watcher)
+        else:
+            logger.error("Bad section %r", section)
 
     # making sure we return consistent lists
     sort_by_field(watchers)
