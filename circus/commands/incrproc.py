bfd463a25c9a9fdf8c8546432b4539b5859a4d12
msempere <msempere@users.noreply.github.com>
2014-12-28 21:26:00 +0000

    Update incrproc.py
    Removing  trailing whitespace

--- circus/commands/incrproc.py
+++ circus/commands/incrproc.py
@@ -8,7 +8,7 @@ class IncrProc(Command):
         Increment the number of processes in a watcher
         ==============================================
 
-        This comment increment the number of processes in a watcher 
+        This comment increment the number of processes in a watcher
         by <nbprocess>, being 1 the default.
 
         ZMQ Message
