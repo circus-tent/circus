3e16747375330a1745881d1651341e53f0444f15
Tasos Vogiatzoglou <tvoglou@iccode.gr>
2015-05-08 22:54:40 +0300

    Recalculate max_age for each process

--- circus/watcher.py
+++ circus/watcher.py
@@ -555,9 +555,9 @@ class Watcher(object):
     @gen.coroutine
     @util.debuglog
     def remove_expired_processes(self):
-        max_age = self.max_age + randint(0, self.max_age_variance)
         expired_processes = [p for p in self.processes.values()
-                             if p.age() > max_age]
+                             if p.age() > (self.max_age + randint(0,
+                                           self.max_age_variance))]
         removes = yield [self.kill_process(x) for x in expired_processes]
         for i, process in enumerate(expired_processes):
             if removes[i]:
