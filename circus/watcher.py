f8917b656056d621be9ec122384d8531cd308bf5
Rafael Viotti <rviotti@gmail.com>
2015-07-01 21:22:19 -0300

    PEP 8 fix on "circus/watcher.py"

--- circus/watcher.py
+++ circus/watcher.py
@@ -269,8 +269,8 @@ class Watcher(object):
                           "stdout_stream_conf", "on_demand",
                           "stderr_stream_conf", "max_age", "max_age_variance",
                           "close_child_stdin", "close_child_stdout",
-                          "close_child_stderr", "use_papa") + \
-                          tuple(options.keys()))
+                          "close_child_stderr", "use_papa") +
+                         tuple(options.keys()))
 
         if not working_dir:
             # working dir hasn't been set
