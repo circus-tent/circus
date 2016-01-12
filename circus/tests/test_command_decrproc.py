96cb47709e263664bf3784deed067d4b71ed50a4
David Douard <david.douard@logilab.fr>
2016-01-12 12:01:16 +0100

    Small refactoring to please flake8

--- circus/tests/test_command_decrproc.py
+++ circus/tests/test_command_decrproc.py
@@ -1,5 +1,4 @@
-from circus.tests.test_command_incrproc import (FakeArbiter,
-        FakeArbiterWithSingletonWatchers)
+import circus.tests.test_command_incrproc as tci
 from circus.tests.support import TestCircus, EasyTestSuite
 from circus.commands.decrproc import DecrProc
 
@@ -8,7 +7,7 @@ class DecrProcTest(TestCircus):
 
     def test_decr_proc(self):
         cmd = DecrProc()
-        arbiter = FakeArbiter()
+        arbiter = tci.FakeArbiter()
         self.assertTrue(arbiter.watchers[0].numprocesses, 1)
 
         props = cmd.message('dummy')['properties']
@@ -17,7 +16,7 @@ class DecrProcTest(TestCircus):
 
     def test_decr_proc_singleton(self):
         cmd = DecrProc()
-        arbiter = FakeArbiterWithSingletonWatchers()
+        arbiter = tci.FakeArbiterWithSingletonWatchers()
         size_before = arbiter.watchers[0].numprocesses
 
         props = cmd.message('dummy', 3)['properties']
