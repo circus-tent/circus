c36c3d8b953af97cc16584bd817b4d35a86dd7ee
Michael Floßmann <michi@kiteswarms.com>
2021-03-15 16:17:39 +0100

    add comments and fix the address resolution

--- circus/util.py
+++ circus/util.py
@@ -693,11 +693,14 @@ def configure_logger(logger, level='INFO', output="-", loggerconfig=None,
         elif output.startswith('syslog://'):
             # URLs are syslog://host[:port]?facility or syslog:///path?facility
             info = urlparse(output)
-            facility = 'user'
+
+            # find out the specified facility
             if info.query in logging.handlers.SysLogHandler.facility_names:
                 facility = info.query
+            else:
+                facility = 'user'
             if info.netloc:
-                address = (info.netloc, info.port or 514)
+                address = (info.hostname, info.port or 514)
             else:
                 address = info.path
             datefmt = LOG_DATE_SYSLOG_FMT
