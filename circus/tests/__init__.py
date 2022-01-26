de5c0b7d4ee5d23a8c62a10b995c38928a775211
Felix Pan <tinylambda@gmail.com>
2022-01-26 18:24:34 +0800

    fix unit tests errors

--- circus/tests/__init__.py
+++ circus/tests/__init__.py
@@ -1,4 +1,6 @@
 import os
+
+import papa
 import zmq
 from circus.util import configure_logger
 from circus import logger
@@ -13,11 +15,16 @@ if not _CONFIGURED and 'TESTING' in os.environ:
 
 def setUp():
     from circus import _patch   # NOQA
+    papa.set_default_port(30303)
+    papa.set_default_connection_timeout(5)
 
 
 def tearDown():
     # There seems to some issue with context cleanup and Python >= 3.4
     # making the tests hang at the end
-    # Explicitely destroying the context seems to do the trick
+    # Explicitly destroying the context seems to do the trick
     # cf https://github.com/zeromq/pyzmq/pull/513
     zmq.Context.instance().destroy()
+
+    with papa.Papa() as p:
+        p.exit_if_idle()
