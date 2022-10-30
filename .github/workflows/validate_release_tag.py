3bed2f90f4969921f97bb954ea67830c00bfdeef
Sebastiaan Huber <mail@sphuber.net>
2022-10-30 17:47:19 +0100

    Update .github/workflows/validate_release_tag.py
    Co-authored-by: Ivan Elfimov <ielfimov@gmail.com>

--- .github/workflows/validate_release_tag.py
+++ .github/workflows/validate_release_tag.py
@@ -30,7 +30,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
     tag_prefix = 'refs/tags/v'
     assert args.GITHUB_REF.startswith(tag_prefix), f'GITHUB_REF should start with "{tag_prefix}": {args.GITHUB_REF}'
-    tag_version = args.GITHUB_REFremoveprefix(tag_prefix)
+    tag_version = args.GITHUB_REF.removeprefix(tag_prefix)
     package_version = get_version_from_module(Path('circus/__init__.py').read_text(encoding='utf-8'))
     error_message = f'The tag version `{tag_version}` is different from the package version `{package_version}`'
     assert tag_version == package_version, error_message
